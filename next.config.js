5fb0e0b4d13c19a20f01b91857fbe45b386b5d87
AndresDrimer <andresdrimer@hotmail.com>
2023-07-11 10:01:57 -0300

    api_url update

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
         DB_USER: process.env.DB_USER,
         DB_PASSWORD: process.env.DB_PASSWORD,
         DB_NAME: process.env.DB_NAME,
+        API_URL: process.env.API_URL
     },
 }
 
